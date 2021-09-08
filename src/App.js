b003624e22aaeb8b0e96693e549115c372c8f54b
mdee <design4web2014@gmail.com>
2021-09-08 11:13:08 +0200

    adding tests for App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import {
   Container,
   Typography,
@@ -13,6 +13,7 @@ import {
   ListItemIcon,
   ListItemText,
   ButtonGroup,
+  Box,
 } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
@@ -39,63 +40,87 @@ class App extends Component {
     this.fetchJobs();
   }
 
+  handleTabBtn = (index) => {
+    this.setState({ value: index });
+  };
+
   render() {
     const { value } = this.state;
 
     const { company, dates, duties, title } = this.state.jobs[value] || {};
     return (
-      <Container>
-        <Grid container spacing={4} justifyContent="center" alignItems="center">
-          <Grid item xs={12} sm={2}>
-            <ButtonGroup orientation="vertical" color="primary">
-              {this.state.jobs.map((jobItem, index) => {
-                return (
-                  <Button
-                    key={jobItem.id}
-                    onClick={() => this.setState({ value: index })}
-                    variant={index === this.state.value && "contained"}
-                  >
-                    {jobItem.company}
+      <Container data-test="component-app">
+        {this.state.loading ? (
+          <Grid container justifyContent="center" alignItems="center">
+            <Grid item>
+              <Box mt={10}>
+                <CircularProgress />
+              </Box>
+            </Grid>
+          </Grid>
+        ) : (
+          <Grid
+            container
+            spacing={4}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={12} sm={2}>
+              <ButtonGroup
+                orientation="vertical"
+                color="primary"
+                data-test="btn-container"
+              >
+                {this.state.jobs.map((jobItem, index) => {
+                  return (
+                    <Button
+                      className="foo"
+                      key={jobItem.id}
+                      onClick={() => this.handleTabBtn(index)}
+                      variant={index === this.state.value && "contained"}
+                    >
+                      {jobItem.company}
+                    </Button>
+                  );
+                })}
+              </ButtonGroup>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Card>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {company}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    {dates}
                   </Button>
-                );
-              })}
-            </ButtonGroup>
+                </CardActions>
+                <List data-test="duties-container">
+                  {duties &&
+                    duties.map((duty, index) => (
+                      <ListItem key={index}>
+                        <ListItemIcon>
+                          <KeyboardArrowRightIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={duty} />
+                      </ListItem>
+                    ))}
+                </List>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={10}>
-            <Card>
-              <CardActionArea>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {title}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {company}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  {dates}
-                </Button>
-              </CardActions>
-              <List>
-                {duties &&
-                  duties.map((duty, index) => (
-                    <ListItem key={index}>
-                      <ListItemIcon>
-                        <KeyboardArrowRightIcon />
-                      </ListItemIcon>
-                      <ListItemText primary={duty} />
-                    </ListItem>
-                  ))}
-              </List>
-            </Card>
-          </Grid>
-        </Grid>
+        )}
       </Container>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow, mount } from "enzyme";
+import sinon from "sinon";
+import { Button, ButtonGroup } from "@material-ui/core";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import App from "./App";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+const mockTabButton = jest.fn();
+
+const initialState = {
+  loading: true,
+};
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test("initial state of App component", () => {
+  const wrapper = shallow(<App />);
+  const initialLoadingState = wrapper.state("loading");
+  const initialJobsState = wrapper.state("jobs");
+  const initialValueState = wrapper.state("value");
+  expect(initialLoadingState).toBe(true);
+  expect(initialJobsState).toEqual([]);
+  expect(initialValueState).toBe(0);
+});
+
+test("calls componentDidMount", () => {
+  const wrapper = shallow(<App />);
+  const instance = wrapper.instance();
+  jest.spyOn(instance, "fetchJobs");
+  instance.componentDidMount();
+  expect(instance.fetchJobs).toHaveBeenCalledTimes(1);
+});
+
+test("length of tab buttons", () => {
+  const wrapper = mount(<App {...initialState} />);
+  wrapper.setState({ loading: false });
+  expect(wrapper.find("[data-test='btn-container']").children()).toHaveLength(
+    2
+  );
+});
+
+test("length of duties in Card", () => {
+  const wrapper = mount(<App {...initialState} />);
+  wrapper.setState({ loading: false });
+  expect(
+    wrapper.find("[data-test='duties-container']").children()
+  ).toHaveLength(2);
 });
